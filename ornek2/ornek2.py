97d010e6a40103057f5bd90f6ee20c07b909d7ca
naimmustafa <naimmetinmustafa@gmail.com>
2021-04-09 23:24:30 +0100

    ornek 2

---
+++ ornek2/ornek2.py
@@ -0,0 +1,16 @@
+
+def sumOddLengthSubarrays(self, arr):
+    """
+    :type arr: List[int]
+    :rtype: int
+    """     
+
+arr = [1,2,5,6,6]
+
+arr = [1,1,1,1,0,0,1,0,1,0,1,1]
+
+arr = [132,423,43,234,554,864,256]
+
+arr = [0,0,0,0,0]
+
+arr = [100000, 200000, 23423543]
